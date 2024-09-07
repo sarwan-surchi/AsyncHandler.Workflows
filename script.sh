7e4752e73b8c76a992621d9bff7ce7576e8c2308
Sarwan <s.surchi@outlook.com>
2024-09-07 17:51:39 +0300

    test the workflow

--- script.sh
+++ script.sh
@@ -1,11 +1,5 @@
 
-
-echo "Creating asynchandler database..................."
-
-echo "checking for azuresqlenv: $azuresqlenv"
-echo "checking for mssqlenv: $mssqlenv"
-
-echo "running script for mssql db: $mssql_db"
+echo "running script for mssql db creation: $mssql_db"
 
 sqlcmd -s localhost -U sa -P $mssql_password -q "create database $mssql_db;"
 
